e76394a3030b16e42e6289b41c08240fbb5d5473
Robert Udrea <udrearobert999@gmail.com>
2023-03-26 18:39:54 +0300

    Added : - Refined margins / paddings / alignment / justify of the elements - Added logout dropdown - Added logout functionality from JS

---
+++ PinPoint/wwwroot/js/pages-partial/nav-login-partial.js
@@ -0,0 +1,25 @@
+﻿const navAvatar = document.getElementById('nav-avatar');
+const navAvatarDropDown = document.getElementById('nav-avatar-dropdown')
+
+window.addEventListener('click', (e) => {
+    if (!navAvatar.contains(e.target))
+        navAvatarDropDown.hidden = true;
+})
+
+navAvatar.addEventListener('click', () => {
+    navAvatarDropDown.toggleAttribute('hidden');
+})
+
+document.getElementById('logout-action').addEventListener('click', async () => {
+    const respone = await fetch(`/Identity/Account/Logout`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'RequestVerificationToken': getAntiForgeryToken()
+        },
+    })
+
+    if (respone.ok)
+        window.location.href = '/'; // redirect to main page
+})
